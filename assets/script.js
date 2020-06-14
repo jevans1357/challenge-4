71acf0fae9774a72f6c8e72605f78488d4f028b6
jevans1357 <64973043+jevans1357@users.noreply.github.com>
2020-06-14 16:31:46 -0700

    timer and score functionality

--- assets/script.js
+++ assets/script.js
@@ -3,57 +3,77 @@ const answer2 = document.getElementById("btn-2")
 const answer3 = document.getElementById("btn-3")
 const answer4 = document.getElementById("btn-4")
 const start_button = document.getElementById("start_button")
+const submit_button = document.getElementById("submit_initials")
+
+var index = 0
+var totalCorrect = 0
+var duration = 90
+var timer = duration, minutes, seconds;
+
+function startTimer(display) {
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        
+        if (--timer <= -2) {
+            endGame();
+        }   else {
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+                display.textContent = minutes + ":" + seconds;
+            }
+    }, 1000);
+}
 
 const questions = [
     {
         question: "Where is Bangkok?",
         answers: [{
-            answerName: "USA", correct: false}, {
-            answerName: "India", correct: false}, {
-            answerName: "Japan", correct: false}, {
-            answerName: "Thailand", correct: true}    
+            answerName: "1. USA", correct: false}, {
+            answerName: "2. India", correct: false}, {
+            answerName: "3. Japan", correct: false}, {
+            answerName: "4. Thailand", correct: true}    
         ]
     },
     {
         question: "Where is Carmen San Diego?",
         answers: [{
-            answerName: "Alaska", correct: false}, {
-            answerName: "California", correct: true}, {
-            answerName: "New York", correct: false}, {
-            answerName: "Alabama", correct: false}    
+            answerName: "1. Alaska", correct: false}, {
+            answerName: "2. California", correct: true}, {
+            answerName: "3. New York", correct: false}, {
+            answerName: "4. Alabama", correct: false}    
         ]
     }, 
     {
      question: "Where is Las Vegas?",
      answers: [{
-        answerName: "USA", correct: true}, {
-        answerName: "India", correct: false}, {
-        answerName: "Japan", correct: false}, {
-        answerName: "Thailand", correct: false}    
+        answerName: "1. USA", correct: true}, {
+        answerName: "2. India", correct: false}, {
+        answerName: "3. Japan", correct: false}, {
+        answerName: "4. Thailand", correct: false}    
         ]
     },
     {
         question: "Where is Tokyo?",
         answers: [{
-        answerName: "USA", correct: false}, {
-        answerName: "India", correct: false}, {
-        answerName: "Japan", correct: true}, {
-        answerName: "Thailand", correct: false}    
+        answerName: "1. USA", correct: false}, {
+        answerName: "2. India", correct: false}, {
+        answerName: "3. Japan", correct: true}, {
+        answerName: "4. Thailand", correct: false}    
         ]
     }, 
     {
         question: "Where is Taj Mahal?",
         answers: [{
-        answerName: "USA", correct: false}, {
-        answerName: "India", correct: true}, {
-        answerName: "Japan", correct: false}, {
-        answerName: "Thailand", correct: false}    
+        answerName: "1. USA", correct: false}, {
+        answerName: "2. India", correct: true}, {
+        answerName: "3. Japan", correct: false}, {
+        answerName: "4. Thailand", correct: false}    
         ]
     }
 ]
 
-var index = 0
-var totalCorrect = 0
 
 var answerQuestion = function(_str) {
     block = questions[index]
@@ -65,25 +85,26 @@ var answerQuestion = function(_str) {
         totalCorrect++
     } else if(_str == "btn-4" && block.answers[3].correct){
         totalCorrect++
+    } else {
+        timer -= 5;
     }
     if(index < questions.length){
         index += 1
         if(index == questions.length) {
-            document.getElementById("question").setAttribute("style","display:none")
-            answer1.setAttribute("style", "display: none")
-            answer2.setAttribute("style", "display: none")
-            answer3.setAttribute("style", "display: none")
-            answer4.setAttribute("style", "display: none")
-
-            document.getElementById("score").setAttribute("style", "")
-            document.getElementById("total_score").setAttribute("style", "")
-            document.getElementById("total_score").innerText = totalCorrect
+          endGame()
         }  else {
             updateQuestion()
         }
     }
 }
 
+var endGame = function() {
+    document.getElementById("answer-buttons").setAttribute("style", "display: none")
+    document.getElementById("submit").setAttribute("style", "")
+    document.getElementById("total_score").innerText = totalCorrect
+   
+    document.getElementById("timer").setAttribute("style", "display: none")
+}
 
 var updateQuestion = function() {
     block = questions[index]
@@ -107,12 +128,53 @@ answer4.addEventListener("click", function () {
     answerQuestion("btn-4")
 })
 start_button.addEventListener("click", function () {
-    document.getElementById("question").setAttribute("style","")
     document.getElementById("answer-buttons").setAttribute("style","")
     start_button.setAttribute("style", "display: none")
-    document.getElementById("hello", "display: none" )
     updateQuestion()
+    startTimer(document.getElementById('timer'))
 })
 
+submit_button.addEventListener("click", function () {
+
+    if(window.localStorage.getItem("highScores")==null) {
+        window.localStorage.setItem("highScores",JSON.stringify({}))
+    } 
+    var initialsObject = JSON.parse(window.localStorage.getItem("highScores"))
+    var initials = document.getElementById("myInput").value
+    if(initials == ""){
+        return
+    }
+    if(initials in initialsObject){
+        if(initialsObject[initials] < totalCorrect){
+            initialsObject[initials] = totalCorrect
+        }
+    } else {
+        initialsObject[initials] = totalCorrect
+    }
+    window.localStorage.setItem("highScores", JSON.stringify(initialsObject))
+    console.log(window.localStorage.getItem("highScores"))
+    document.getElementById("submit").setAttribute("style", "display: none;")
+    displayHighScores(initialsObject) 
+})
+
+function displayHighScores(initialsObject){
+    document.getElementById("highScores").setAttribute("style", "")
+    var sortable = [];
+    Object.keys(initialsObject).forEach((key, index) => {
+       sortable.push(key) 
+    })
+    sortable.sort(function(a,b) {
+        return initialsObject[b] - initialsObject[a]
+    });
+    var high_scores_list = document.getElementById("highScores");
+    sortable.forEach((key) => {
+        var lst_entry = document.createElement("li");
+        var textNode = document.createTextNode(key + " " + initialsObject[key]);
+        lst_entry.appendChild(textNode);
+        high_scores_list.appendChild(lst_entry);
+    });
+
+}
 
 
+window.alert("Let's take a quiz!")
